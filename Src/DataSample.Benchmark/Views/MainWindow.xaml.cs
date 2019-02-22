197eeec2ee4accb9a4b04169f9b8c605feda65ef
Riccardo Cappello <rcappello@outlook.com>
2019-02-22 15:38:36 +0100

    Benchmark demo app

--- Src/DataSample.Benchmark/Views/MainWindow.xaml.cs
+++ Src/DataSample.Benchmark/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DataSample.Benchmark
+namespace DataSample.Benchmark.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -28,6 +28,84 @@ namespace DataSample.Benchmark
         private void ButtonExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private async void ButtonClearAndInit_Click(object sender, RoutedEventArgs e)
+        {
+            LogArea.Text = string.Empty;
+            needleDapper.Value = 0.0f;
+            needleEntityFramework.Value = 0.0f;
+            scaleDapper.Max = 2000;
+            scaleEntityFramework.Max = 2000;
+
+            var elapsedMilliseconds = await MeasureExecutionAsync(async () =>
+            {
+                await App.DapperProductsService.GetAsync(string.Empty, 0, 100);
+                await App.EntityFrameworkProductsService.GetAsync(string.Empty, 0, 100);
+            });
+            LogArea.Text = "Ready";
+        }
+
+
+        protected async Task<long> MeasureExecutionAsync(Func<Task> action)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            try
+            {
+                Cursor = Cursors.Wait;
+                await action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Cursor = Cursors.Arrow;
+            }
+            watch.Stop();
+            var elapsedMS = watch.ElapsedMilliseconds;
+
+            return elapsedMS;
+        }
+
+        private async Task ExecuteTestAsync(float maxValue, int maxRecords)
+        {
+            needleDapper.Value = needleEntityFramework.Value = 0.0f;
+            scaleDapper.Max = scaleEntityFramework.Max = maxValue;
+
+            var elapsedMilliseconds = await MeasureExecutionAsync(async () =>
+            {
+                await App.DapperProductsService.GetAsync(string.Empty, 0, maxRecords);
+            });
+            needleDapper.Value = elapsedMilliseconds;
+            LogArea.Text += $"\nDapper {maxRecords}: {elapsedMilliseconds}ms";
+
+            elapsedMilliseconds = await MeasureExecutionAsync(async () =>
+            {
+                await App.EntityFrameworkProductsService.GetAsync(string.Empty, 0, maxRecords);
+            });
+
+            needleEntityFramework.Value = elapsedMilliseconds;
+            LogArea.Text += $"\nEntity Framework Core {maxRecords}: {elapsedMilliseconds}ms";
+        }
+
+        private async void Button1000Records_Click(object sender, RoutedEventArgs e)
+        {
+            await ExecuteTestAsync(2000, 1000);
+        }
+
+        private async void Button10000Records_Click(object sender, RoutedEventArgs e)
+        {
+            await ExecuteTestAsync(2000, 10000);
+        }
+        private async void Button50000Records_Click(object sender, RoutedEventArgs e)
+        {
+            await ExecuteTestAsync(2000, 50000);
+        }
+        private async void Button100000Records_Click(object sender, RoutedEventArgs e)
+        {
+            await ExecuteTestAsync(2000, 100000);
         }
     }
 }
